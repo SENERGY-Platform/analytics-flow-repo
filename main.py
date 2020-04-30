2ab1ddec3907408dfed16b7a2c40d9e896ede3ab
TZ <zsco@users.noreply.github.com>
2020-04-30 16:10:46 +0200

    add share fields

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# Copyright 2018 InfAI (CC SES)
+#  Copyright 2018 InfAI (CC SES)
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 import datetime
 import os
 
@@ -45,11 +45,18 @@ model = api.model('Model', {
     'cells': fields.Raw,
 })
 
+share = api.model('Share', {
+    'list': fields.Boolean,
+    'read': fields.Boolean,
+    'write': fields.Boolean
+})
+
 flow_model = api.model('Flow', {
     'name': fields.String(required=True, description='Flow name'),
     'description': fields.String(required=True, description='Flow description'),
     'model': fields.Nested(model),
-    'image': fields.String(required=False, description='Flow image')
+    'image': fields.String(required=False, description='Flow image'),
+    'share': fields.Nested(share)
 })
 
 flow_return = flow_model.clone('Flow', {
